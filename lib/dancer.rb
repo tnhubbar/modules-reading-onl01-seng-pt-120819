05de48933df25c3432bc9b13370792a7113c6eec
Taryn Johnson <taryn.johnson919@gmail.com>
2020-01-26 02:35:20 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -6,4 +6,11 @@ require_relative './fancy_dance.rb'
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+  
 end
