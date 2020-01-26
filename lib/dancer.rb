e8423deb542d57a2bc0256b9b485ac62abb71612
Taryn Johnson <taryn.johnson919@gmail.com>
2020-01-26 02:22:17 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,13 +3,6 @@ require_relative './dance_module.rb'
 
 
 class Dancer
-  include Dance
-  extend MetaDancing
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end 
-
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
